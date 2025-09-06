09a4fd34f0b345f8be72bbdeac4e6e07fb2fbf97
mHardhik <hardhikm.08@gmail.com>
2025-09-06 14:33:50 +0530

    Add files via upload

---
+++ frontend/pages/sitemap.xml.tsx
@@ -0,0 +1,48 @@
+import { GetServerSideProps } from 'next'
+
+const Sitemap = () => {
+  return null
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
+
+  const staticPages = [
+    '',
+    '/about',
+    '/blog',
+    '/privacy-policy',
+    '/terms-of-service',
+    '/acceptable-use-policy',
+    '/auth/signin',
+    '/dashboard',
+    '/profile'
+  ]
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${staticPages
+        .map((path) => {
+          return `
+            <url>
+              <loc>${baseUrl}${path}</loc>
+              <lastmod>${new Date().toISOString()}</lastmod>
+              <changefreq>daily</changefreq>
+              <priority>0.7</priority>
+            </url>
+          `
+        })
+        .join('')}
+    </urlset>
+  `
+
+  res.setHeader('Content-Type', 'text/xml')
+  res.write(sitemap)
+  res.end()
+
+  return {
+    props: {},
+  }
+}
+
+export default Sitemap
